abe068619a500e7391ae031bc17f6d920a0eaef3
vignesh-prakash <vignesh24p@gmail.com>
2024-10-31 19:17:14 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -16,6 +16,7 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert(result == 22);
     printf("All is well (maybe!)\n");
     return 0;
 }
